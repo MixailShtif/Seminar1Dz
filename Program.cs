7ffccda4a5e79b05e7efb34e482ec70151c317d4
MixailShtif <136107544+MixailShtif@users.noreply.github.com>
2023-06-26 18:57:09 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
-﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7   -> 7
-// 44, 5, 78 -> 78
-// 22, 3, 9  -> 22
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+//    a = 5;   b = 7   -> max = 7
+//    a = 2;   b = 10  -> max = 10
+//    a = -9;  b = -3  -> max = -3
 
 Console.Clear();
 Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-int number3 = Convert.ToInt32(Console.ReadLine());
-int max = number1;
-if (number2 > max) max = number2;
-if (number3 > max) max = number3;
-Console.WriteLine($"max = {max}");
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}");
+}
